c11e366242a0c47b2a67fb0d4fd14d4e4797cb70
howaboutdale <1daletromp@gmail.com>
2023-05-12 10:09:57 +0930

    Create Sheet Page, now functional

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -5,7 +5,7 @@ home_routes = Blueprint('home_routes', __name__)
 
 home_routes.route('')(index)
 home_routes.route('/new')(new)
-home_routes.route('', methods=['POST'])(create)
+home_routes.route('', methods=["POST"])(create)
 home_routes.route('/<id>/edit')(edit)
 home_routes.route('/<id>', methods=["POST"])(update)
 home_routes.route('/<id>/delete', methods=["POST"])(delete)
